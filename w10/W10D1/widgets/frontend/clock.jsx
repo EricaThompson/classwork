0727771e810141c69dc807a54b0d625283047556
“ericathompson” <ericathompson.nc@gmail.com>
2020-11-23 17:19:28 -0800

    Add functioning Clock component

--- w10/W10D1/widgets/frontend/clock.jsx
+++ w10/W10D1/widgets/frontend/clock.jsx
@@ -3,14 +3,39 @@ import ReactDOM from 'react-dom';
 
 
 class Clock extends React.Component {
+    constructor(props){
+        super(props),
+        this.state = {
+            date: new Date()
+        }
+    }
 
-  render() {
-    return (
-      <div>
-        <h1>Clock</h1>
-      </div>
-    )
-  }
+    tick(){
+        this.setState({date: new Date()})
+    }
+
+    componentDidMount() {
+        this.tickID = setInterval(()=> this.tick(), 1000) //sets to current time
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.tickID);
+        this.tickID = 0;
+    }
+
+    render() {
+        return (
+        <div>
+            <h1>Clock</h1>
+            <ul>
+                <li>{this.state.date.getHours()}</li>
+                <li>{this.state.date.getMinutes()}</li>
+                <li>{this.state.date.getSeconds()}</li>
+            </ul>
+        </div>
+
+        )
+    }
 
 }
 
