7cd2805e8df28d402b5a479f9cdafd7d8cc20e5e
“ericathompson” <ericathompson.nc@gmail.com>
2020-12-01 23:35:34 -0800

    Add pokedex.jsx

--- w11/W11D1/pokedex/webpack.config.js
+++ w11/W11D1/pokedex/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/pokedex.jsx',
+  entry: './frontend/components/pokedex.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
@@ -17,7 +17,7 @@ module.exports = {
         exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
-          query: {
+          options: {
             presets: ['@babel/env', '@babel/react']
           }
         },
