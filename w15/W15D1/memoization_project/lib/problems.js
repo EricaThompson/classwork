ebd26dabafd3a926ace02c106f862cab43d2581f
“ericathompson” <ericathompson.nc@gmail.com>
2021-01-04 13:08:40 -0800

    Add EOD

--- w15/W15D1/memoization_project/lib/problems.js
+++ w15/W15D1/memoization_project/lib/problems.js
@@ -95,9 +95,27 @@ function lucasNumberRecursiveSolution(n){
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
 function minChange(coins, amount, memo = {}) {
 
-    
+    let change = []
+
+    for (let i = 1; i < coins.length; i++){
+        if (coins[coins.length - i] < amount){
+            change.push(coins[coins.length - i])
+            if (coins[coins.length - i] < (amount - change.reduce((a, b) => a + b))){
+                //1 
+                change.push(coins[coins.length - i])
+            } 
+        }
+
+    }
+
+    return change.length;
 
 }
+
+console.log(minChange([1, 2, 5], 11))         // => 3, because 5 + 5 + 1 = 11
+console.log(minChange([1, 4, 5], 8))         // => 2, because 4 + 4 = 8
+console.log(minChange([1, 5, 10, 25], 15))    // => 2, because 10 + 5 = 15
+console.log(minChange([1, 5, 10, 25], 100))   // => 4, because 25 + 25 + 25 + 25 = 100
 
 
 module.exports = {
