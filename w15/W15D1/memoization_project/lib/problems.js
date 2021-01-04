73af00a72e187d0f6520db79b5d823f5bcd89e60
“ericathompson” <ericathompson.nc@gmail.com>
2021-01-04 11:54:09 -0800

    Complete Lucas number memoization

--- w15/W15D1/memoization_project/lib/problems.js
+++ w15/W15D1/memoization_project/lib/problems.js
@@ -16,8 +16,68 @@
 // lucasNumberMemo(41)  // => 370248451
 // lucasNumberMemo(42)  // => 599074578
 function lucasNumberMemo(n, memo = {}) {
+    // if (n in memo) return memo[n];
+    // // let sequence = [2, 1, 3, 4, 7, 11]
+    
+    // if (n === 0) return 2;
+    // if (n === 1) return 1;
+    
+    // // sequence.map((num, idx) =>{
+    // //     memo[idx] = num
+    // // })
+
+    // let answer = memo[n - 1] + memo[n - 2] + lucasNumberMemo(n - 1, memo)
+    
+    // memo[n] = answer
+    
+    // return answer
+    
+    // // if (n in memo) return memo[n];
+
+    if (n in memo) return memo[n];
+
+    if (n === 0) return 2;
+    if (n === 1) return 1;
+
+    memo[n] = lucasNumberMemo(n - 1, memo) + lucasNumberMemo(n - 2, memo)
+
+    return memo[n]
 
 }
+
+// console.log(lucasNumberMemo(18))
+
+function lucasNumberRecursive(n){
+    if (n === 0) return 2;
+    if (n === 1) return 1;
+
+    let sequence = [2, 1]
+
+    if (n - 1 in sequence) return sequence[sequence.length - 1] + sequence[sequence.length - 2]
+    // let distance = n - sequence.length //for 3, n[1] + n[0], place at n - 1 = 2
+    let num1 = sequence[n - 2]
+    let num2 = sequence[n - 3]
+    let answer = num1 + num2
+
+    sequence[n - 1] = answer
+    
+
+    return answer
+
+}
+
+// console.log(lucasNumberRecursive(3))
+
+
+function lucasNumberRecursiveSolution(n){
+    if (n === 0) return 2;
+    if (n === 1) return 1;
+
+    return lucasNumberRecursiveSolution(n-1) + lucasNumberRecursiveSolution(n-2)
+
+}
+
+// console.log(lucasNumberRecursiveSolution(18))
 
 
 // Write a function, minChange(coins, amount), that accepts an array of coin values
@@ -34,6 +94,8 @@ function lucasNumberMemo(n, memo = {}) {
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
 function minChange(coins, amount, memo = {}) {
+
+    
 
 }
 
